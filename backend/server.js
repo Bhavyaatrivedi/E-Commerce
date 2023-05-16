9523f7b8e2742666073cb60146839cb64d030e84
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-16 22:55:50 +0530

    config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const app = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config({path:"backend/config/config.env"})
 app.listen(process.env.PORT, () =>{
     console.log(`Server is working on http://localhost:$(process.env.PORT)`)
 })
