aac688acac476715725440a601d4a18e628eb7c6
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-17 23:31:45 +0530

    create product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,19 @@
+const Product = require("../modals/productModal")
+
+
+
+//Create Product ..Admin
+
+exports.createProduct = async(req,res,next)=>{
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+//Get all Product
 exports.getAllProducts = (req,res) =>{
     res.status(200).json({message:"route is working"})
 }
