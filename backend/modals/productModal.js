1e8a2973b156f45c4e3a3e2343d108101eaa5697
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-17 23:09:44 +0530

    product modal

--- backend/modals/productModal.js
+++ backend/modals/productModal.js
@@ -32,6 +32,39 @@ const productSchema = new mongoose.Schema({
 
     ],
     category:{
-
+        type: String,
+        requuired:[true, "Please enter Product category"],
+    },
+    Stock:{
+        type:Number,
+        required:[true, "Please enter product stock"],
+        maxLength:[4, "Stock cannot exceed 4 charcters"],
+        default:1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true,
+            },
+            rating:{
+                type:Number,
+                required:true,
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
     }
 })
+
+module.exports = mongoose.model("Product", productSchema);
