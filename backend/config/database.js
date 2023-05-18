245f1db3d54e7f20c214a5d7d6b8f38fa7d11164
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-18 23:51:35 +0530

    uncaught errors

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,7 @@ const connectDB = (url) => {
       useUnifiedTopology: true,
     })
     .then(() => console.log("Connected to DB"))
-    .catch((err) => console.log(err));
+    
 };
 
 module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,14 @@
-// require("dotenv").config();
-// const express = require("express");
-// // const dotenv = require("dotenv");
-// const app = require("./app");
-// const connectDatabase= require("./config/database")
-
-// //connect to  database
-// connectDatabase()
-// // dotenv.config({ path: "backend/config/config.env" });
-
-
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server is running on http://localhost:${process.env.PORT}`);
-// });
-
-
-
 require("dotenv").config();
 const app = require("./app");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-// const connectDB = require("./config/connect");
 
+//Handling Ucaught exception
+process.on("uncaughtException", (err)=>{
+  console.log(`Error : ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught exception`)
+  process.exit(1);
+})
 //config OF DOTENV
 dotenv.config({ path: "backend/config/config.env" });
 
@@ -34,6 +22,7 @@ mongoose
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
+
 
 // unhandles promise rejection
 process.on("unhandledRejection", (err)=>{
