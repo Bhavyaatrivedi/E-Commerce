e6249688ebb165daaf1f4ac43d6fbbd4686a7e15
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-20 09:23:31 +0530

    pagination

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -15,6 +15,19 @@ class ApiFeatures {
          this.query = this.query.find({...keyword});
          return this;
     }
+
+    //filter
+    filter(){
+       const queryCopy = {...this.queryStr};
+       
+       //removing some fileds
+       const removedFields = ["keywords", "page", "limit"];
+
+       removedFields.forEach(key =>delete queryCopy[key]);
+
+       this.query = this.query.find(queryCopy);
+       return this;
+    }
 }
 
 module.exports = ApiFeatures;
