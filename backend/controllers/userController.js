4e6db20a1699f9ecc65d8c98b258cd9b350dd931
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-21 22:44:52 +0530

    logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,4 +44,17 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     
     sendToken(user,200,res);
 
+});
+
+//Logout user
+exports.logout = catchAsyncErrors(async(req,res,next)=>{
+
+  req.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  })
+  res.status(200).json({
+    success:true,
+    message:"Logges Out"
+  })
 })
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,17 @@
+const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
+const jwt = require("jsonwebtoken")
+const User= require("../models/userModel")
 
 exports.isAuthenticatedUser = catchAsyncErrors( async(req,res,next)=>{
-    const token = req.cookie;
-    console.log(token);
+    const {token} = req.cookie;
+    
+    if(!token){
+        return next(new ErrorHandler("please login to access this resource", 401))
+    }
+
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decodedData.id)
+    next()
 })
