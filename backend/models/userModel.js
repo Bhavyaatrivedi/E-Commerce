0ebd9b1f7b5ff95177940cc6d0312d3d411468f7
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-31 23:07:01 +0530

    reset password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please Enter Your Name"],
     maxLength: [30, "Name cannot exceed 30 characters"],
     minLength: [4, "Name should have more than 4 characters"],
-  },
+  }, 
   email: {
     type: String,
     required: [true, "Please Enter Your Email"],
